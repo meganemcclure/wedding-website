903623af15f35018290721f136c8f50f80b19690
Megan McClure <meganemcclure@gmail.com>
2025-01-26 18:30:38 -0500

    remove unneeded github actions workflows and change date to year, month, day

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex flex-col items-center">
           <p className={"animate-appearslow text-5xl md:text-7xl lg:text-8xl text-primary -mb-4 md:-mb-6 lg:-mb-9"} style={{ fontFamily: 'Italianno, serif' }}>{`Save The Date`}</p>
-          <h1 className={"animate-appearslow  text-7xl md:text-8xl lg:text-9xl"} style={{ fontFamily: 'Instrument Serif, serif' }}>{`08.01.2026`}</h1>
+          <h1 className={"animate-appearslow  text-7xl md:text-8xl lg:text-9xl"} style={{ fontFamily: 'Instrument Serif, serif' }}>{`2026.08.01`}</h1>
           <p className={"text-wrap max-w-[450px] text-center pt-20"} style={{ fontFamily: 'montserrat, sans-serif'}}>{`Welcome to Brian and Megan's wedding website. We're so excited to have you join us on our special day! Stay tuned for more details to come.`}</p>
         </div>
 
