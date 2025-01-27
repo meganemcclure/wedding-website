45e60324210e8599266e145a7eae74d72e55b5d1
Megan McClure <meganemcclure@gmail.com>
2025-01-26 19:35:07 -0500

    add back basepath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",  // <=== enables static exports
-  // basePath: "/wedding-website"
+  basePath: "/wedding-website"
   // reactStrictMode: true,
 };
 
