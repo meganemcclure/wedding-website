29b457ec474b67d8182f06e1f42bffb45e18f5c2
Megan McClure <meganemcclure@gmail.com>
2025-05-04 13:42:14 -0400

    fix linter error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 // import Link from "next/link";
 import logo from "@/public/logo1.svg";
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 // import { Button } from "@/components/ui/button"
 // import {
 //   Sheet,
