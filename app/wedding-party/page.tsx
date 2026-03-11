811bbfb3770174bab203438e9fa0ceff7f504cad
Megan McClure <meganemcclure@gmail.com>
2026-03-11 16:41:27 -0400

    add formatting for wedding party page

--- app/wedding-party/page.tsx
+++ app/wedding-party/page.tsx
@@ -1,131 +1,252 @@
-// import Image from "next/image";
+import { Separator } from "@radix-ui/react-separator";
+import BrianAndMeg from "@/public/Megan_and_Brian_Engagement_aKaiserPhoto_Feb_1_2026-75.jpg";
+import irena from "@/public/irena.jpg";
 
-// const Profile = ({
-// 	title,
-// 	name,
-// 	description,
-// 	img,
-// 	imgAlt,
-// 	className,
-// }: {
-// 	name: string;
-// 	title: string;
-// 	description?: string;
-// 	img: string;
-// 	imgAlt: string;
-// 	className?: string;
-// }) => {
-// 	return (
-// 		<div className={`w-full flex flex-col items-center gap-4 ${className}`}>
-// 			<Image src={img} alt={imgAlt} className="bg-white h-20 w-32" />
-// 			<span className="w-full flex flex-col">
-// 				<h1 className="text-4xl">{name}</h1>
-// 				<h2 className="text-lg">{title}</h2>
-// 				{description && <p>{description}</p>}
-// 			</span>
-// 		</div>
-// 	);
-// };
+const BRIDESMAIDS = [
+	{
+		name: "Lillian McClure",
+		title: "Maid of Honour",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: (
+			<img
+				src={BrianAndMeg.src}
+				alt={""}
+				className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+			/>
+		),
+		imgAlt: "",
+	},
+	{
+		name: "Ashley Piché",
+		title: "Bridesmaid",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: (
+			<img
+				src={BrianAndMeg.src}
+				alt={""}
+				className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+			/>
+		),
+		imgAlt: "",
+	},
+	{
+		name: "Breanna Piché",
+		title: "Bridesmaid",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: (
+			<img
+				src={BrianAndMeg.src}
+				alt={""}
+				className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+			/>
+		),
+		imgAlt: "",
+	},
+	{
+		name: "Irena Dunjic",
+		title: "Bridesmaid",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: <img src={irena.src} alt={""} />,
+		imgAlt: "",
+	},
+];
+
+const GROOMSMEN = [
+	{
+		name: "Trey Ricci",
+		title: "Best Man",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: (
+			<img
+				src={BrianAndMeg.src}
+				alt={""}
+				className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+			/>
+		),
+		imgAlt: "",
+	},
+	{
+		name: "Devon Gavin",
+		title: "Groomsman",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: (
+			<img
+				src={BrianAndMeg.src}
+				alt={""}
+				className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+			/>
+		),
+		imgAlt: "",
+	},
+	{
+		name: "Justin Savarie",
+		title: "Groomsman",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: (
+			<img
+				src={BrianAndMeg.src}
+				alt={""}
+				className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+			/>
+		),
+		imgAlt: "",
+	},
+];
+
+const PARENTS = [
+	{
+		name: "Scott & Rebecca McClure",
+		title: "Parents of the bride",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: (
+			<img
+				src={BrianAndMeg.src}
+				alt={""}
+				className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+			/>
+		),
+		imgAlt: "",
+	},
+	{
+		name: "Chuck & Sherry Piché",
+		title: "Parents of the groom",
+		description:
+			"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+		img: (
+			<img
+				src={BrianAndMeg.src}
+				alt={""}
+				className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+			/>
+		),
+		imgAlt: "",
+	},
+];
+
+const Profile = ({
+	title,
+	name,
+	description,
+	img,
+	imgAlt,
+	className,
+}: {
+	name: string;
+	title?: string;
+	description?: string;
+	img?: any;
+	imgAlt: string;
+	className?: string;
+}) => {
+	return (
+		<div
+			className={`w-full flex flex-col items-center justify-start gap-8 ${className}`}
+		>
+			<div className="w-full max-h-[400px] overflow-hidden bg-white">{img}</div>
+			<span className="flex flex-col w-full gap-0">
+				<h1 className="text-4xl">{name}</h1>
+				<h2 className="text-lg text-primary">{title}</h2>
+				{description && <p className="pt-4">{description}</p>}
+			</span>
+		</div>
+	);
+};
 
 export default function FAQPage() {
 	return (
-		<div className="h-fit w-full flex justify-center items-center py-[80px]">
-			{/* <div className="animate-appearfast w-full max-w-3xl grid grid-cols-1 md:grid-cols-2 p-4 bg-background p-4 md:p-10 lg:p-12 rounded-lg drop-shadow-2xl">
+		<div className="h-fit w-full flex justify-center items-start py-auto p-6 py-[120px]">
+			<div className="animate-appearfast w-full max-w-3xl flex flex-col justify-center items-center gap-8 md:gap-12 p-4 bg-background p-4 md:p-10 lg:p-12 rounded-lg drop-shadow-2xl">
+				<h1
+					className={
+						"w-full text-5xl md:text-7xl lg:text-8xl text-primary -mb-4 md:-mb-6 lg:-mb-9"
+					}
+					style={{ fontFamily: "Italianno, serif" }}
+				>{`Who's in the wedding?`}</h1>
+
 				<Profile
 					name={"Megan & Brian"}
 					title={"Bride & groom"}
-					description={"You know them, you love them, it's Megan & Brian."}
-					img={""}
-					imgAlt={""}
-					className="col-span-full max-w-md mx-auto"
+					description={
+						"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+					}
+					img={
+						<img
+							src={BrianAndMeg.src}
+							alt={""}
+							className="scale-[260%] object-[25%_75%] pr-4 pt-4"
+						/>
+					}
+					imgAlt={"Megan & Brian"}
 				/>
 
-				<Separator className="w-full col-span-full" />
-
-				<div className="col-span-full w-full flex justify-center">
-					<h1 className="text-4xl ">Wedding Party</h1>
+				<div className="pt-4 flex flex-col w-full">
+					<h1
+						className="w-full text-5xl md:text-6xl"
+						style={{ fontFamily: "Italianno, serif" }}
+					>
+						Bridesmaids
+					</h1>
+					<Separator className="w-full col-span-full border" />
 				</div>
-				<Profile
-					name={"Lillian (Lilly) McClure"}
-					title={"Maid of honour"}
-					description={"Megan's maid of honour and her younger sister."}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Trey Ricci"}
-					title={"Best man"}
-					description={"Brian's childhood best friend."}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Irena Dunjic"}
-					title={"Bridesmaid"}
-					description={"Megan's university friend."}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Devon Gavin"}
-					title={"Groomsman"}
-					description={"Brian's childhood friend and old roommate."}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Ashley Piché"}
-					title={"Bridesmaid"}
-					description={"Brian's oldest sister."}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Justin Savarie"}
-					title={"Groomsman"}
-					description={"Brian's childhood friend."}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Breanna Piché"}
-					title={"Bridesmaid"}
-					description={"Brian's twin sister."}
-					img={""}
-					imgAlt={""}
-				/>
+				{BRIDESMAIDS.map((person, index) => (
+					<Profile
+						key={index}
+						name={person.name}
+						title={person.title}
+						description={person.description}
+						img={person.img}
+						imgAlt={person.imgAlt}
+					/>
+				))}
 
-				<Separator className="w-full col-span-full" />
-
-				<div className="col-span-full w-full flex justify-center">
-					<h1 className="text-4xl ">Parents of the bride & groom</h1>
+				<div className="pt-4 flex flex-col w-full">
+					<h1
+						className="w-full text-5xl md:text-6xl"
+						style={{ fontFamily: "Italianno, serif" }}
+					>
+						Groomsmen
+					</h1>
+					<Separator className="w-full col-span-full border" />
 				</div>
+				{GROOMSMEN.map((person, index) => (
+					<Profile
+						key={index}
+						name={person.name}
+						title={person.title}
+						description={person.description}
+						img={person.img}
+						imgAlt={person.imgAlt}
+					/>
+				))}
 
-				<Profile
-					name={"Rebecca (Becky) McClure"}
-					title={"Mother of the bride"}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Sherry Piché"}
-					title={"Mother of the groom"}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Scott McClure"}
-					title={"Father of the bride"}
-					img={""}
-					imgAlt={""}
-				/>
-				<Profile
-					name={"Charles (Chuck) Piché"}
-					title={"Father of the groom"}
-					img={""}
-					imgAlt={""}
-				/>
-			</div> */}
+				<div className="pt-4 flex flex-col w-full">
+					<h1
+						className="w-full text-5xl md:text-6xl"
+						style={{ fontFamily: "Italianno, serif" }}
+					>
+						Parents
+					</h1>
+					<Separator className="w-full col-span-full border" />
+				</div>
+				{PARENTS.map((person, index) => (
+					<Profile
+						key={index}
+						name={person.name}
+						title={person.title}
+						description={person.description}
+						img={person.img}
+						imgAlt={person.imgAlt}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
